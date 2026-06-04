151358f941f289ad83f03a803fd353283945ec7f
shannonkwan86 <3406702953@qq.com>
2026-06-05 04:21:51 +0800

    Update blog settings

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -112,8 +112,6 @@ export const siteConfig: SiteConfig = {
         "/assets/desktop-banner/2.webp",
         "/assets/desktop-banner/3.webp",
         "/assets/desktop-banner/4.webp",
-        "/assets/desktop-banner/5.webp",
-        "/assets/desktop-banner/6.webp",
       ], // 桌面横幅图片
       mobile: [
         "/assets/mobile-banner/1.webp",
