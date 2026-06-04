a42ea874440874cd4bfe336135d9c3bf15bd4175
shannonkwan86 <3406702953@qq.com>
2026-06-05 04:17:11 +0800

    Update site URL

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
   title: "ᯤ³ᴳ Notes",
   subtitle: "Shannon’s Channel",
-  siteURL: "https://shannon.pages.dev/", // 请替换为你的站点URL，以斜杠结尾
+  siteURL: "https://shannon-blog-3ro.pages.dev/",
   siteStartDate: "2026-06-05", // 站点开始运行日期，用于站点统计组件计算运行天数
 
   lang: SITE_LANG,
