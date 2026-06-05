8791bf0a48a9bfd62a394c5dad75414046049965
shannonkwan86 <3406702953@qq.com>
2026-06-05 11:10:51 +0800

    Add new music file: ヨルシカ - 藍二乗.mp3

--- src/components/widgets/music-player/constants.ts
+++ src/components/widgets/music-player/constants.ts
@@ -5,51 +5,51 @@ export const STORAGE_KEY_VOLUME = "music-player-volume";
 export const DEFAULT_VOLUME = 0.7;
 
 export const LOCAL_PLAYLIST: Song[] = [
-	{
-		id: 1,
-		title: "口笛で愛は歌えない",
-		artist: "Dazbee",
-		cover: "assets/music/cover/dazbee.webp",
-		url: "assets/music/url/dazbee.mp3",
-		duration: 0,
-	},
-	{
-		id: 2,
-		title: "ひとり上手",
-		artist: "Kaya",
-		cover: "assets/music/cover/hitori.webp",
-		url: "assets/music/url/hitori.mp3",
-		duration: 240,
-	},
-	{
-		id: 3,
-		title: "眩耀夜行",
-		artist: "ス리즈ブーケ",
-		cover: "assets/music/cover/xryx.webp",
-		url: "assets/music/url/xryx.mp3",
-		duration: 180,
-	},
-	{
-		id: 4,
-		title: "春雷の頃",
-		artist: "22/7",
-		cover: "assets/music/cover/cl.webp",
-		url: "assets/music/url/cl.mp3",
-		duration: 200,
-	},
+  {
+    id: 1,
+    title: "ヨルシカ - 藍二乗",
+    artist: "Yorushika",
+    cover: "assets/music/cover/ヨルシカ - 藍二乗.webp",
+    url: "assets/music/url/ヨルシカ - 藍二乗.mp3",
+    duration: 0,
+  },
+  // {
+  // 	id: 2,
+  // 	title: "ひとり上手",
+  // 	artist: "Kaya",
+  // 	cover: "assets/music/cover/hitori.webp",
+  // 	url: "assets/music/url/hitori.mp3",
+  // 	duration: 240,
+  // },
+  // {
+  // 	id: 3,
+  // 	title: "眩耀夜行",
+  // 	artist: "ス리즈ブーケ",
+  // 	cover: "assets/music/cover/xryx.webp",
+  // 	url: "assets/music/url/xryx.mp3",
+  // 	duration: 180,
+  // },
+  // {
+  // 	id: 4,
+  // 	title: "春雷の頃",
+  // 	artist: "22/7",
+  // 	cover: "assets/music/cover/cl.webp",
+  // 	url: "assets/music/url/cl.mp3",
+  // 	duration: 200,
+  // },
 ];
 
 export const DEFAULT_SONG: Song = {
-	title: "Sample Song",
-	artist: "Sample Artist",
-	cover: "/favicon/favicon.ico",
-	url: "",
-	duration: 0,
-	id: 0,
+  title: "Sample Song",
+  artist: "Sample Artist",
+  cover: "/favicon/favicon.ico",
+  url: "",
+  duration: 0,
+  id: 0,
 };
 
 export const DEFAULT_METING_API =
-	"https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r";
+  "https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r";
 export const DEFAULT_METING_ID = "14164869977";
 export const DEFAULT_METING_SERVER = "netease";
 export const DEFAULT_METING_TYPE = "playlist";
