45949a769c082f1ab359840ae5acfad7bb12659f
shannonkwan86 <3406702953@qq.com>
2026-08-10 16:41:27 +0800

    feat: enable giscus comments

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,22 +3,22 @@ import { SITE_LANG } from "./siteConfig";
 
 // 评论系统配置
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
-	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	system: "giscus", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
 		envId: "https://twikoo.vercel.app",
 		lang: SITE_LANG,
 	},
 	giscus: {
-		repo: "your-github-username/your-repo-name",
-		repoId: "your-repo-id",
+		repo: "shannonkwan86/shannon-blog",
+		repoId: "R_kgDOSxXwNw",
 		category: "Announcements",
-		categoryId: "your-category-id",
+		categoryId: "DIC_kwDOSxXwN84DDDmA",
 		mapping: "pathname",
 		strict: "0",
 		reactionsEnabled: "1",
 		emitMetadata: "0",
-		inputPosition: "top",
+		inputPosition: "bottom",
 		theme: "preferred_color_scheme",
 		lang: SITE_LANG,
 		loading: "lazy",
